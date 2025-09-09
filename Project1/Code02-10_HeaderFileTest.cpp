92b67efdbca44bd72d1a3a0edf47da1e45f735b9
wlgus123 <mail30019794@gmail.com>
2025-09-09 11:00:02 +0900

    로또번호 생성기 만들기

--- Project1/Code02-10_HeaderFileTest.cpp
+++ Project1/Code02-10_HeaderFileTest.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
 #include ".\Common.h"
 
+#define Array vector
+
 int main()
 {
-	randomInit(1, 100);
-	println(cookRandom(gen));
+	/*randomInit(1, 100);
+	println(cookRandom(gen));*/
+	// 로또번호 생성기 하나를 만드시오
+	// 형식: [10 5 23 56 23 99]
+
+	Array<int> roto;
+
+	print("[");
+	for(int i = 0; i < 6; i++)
+	{
+		randomInit(1, 45);
+		print(cookRandom(gen));
+	}
+	println("]");
+
 	return 0;
 }
