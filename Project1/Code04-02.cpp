cd56de38525439e4505c3d0a279db89b298a3f3b
wlgus123 <mail30019794@gmail.com>
2025-09-16 10:56:25 +0900

    단순 연결 리스트 출력

---
+++ Project1/Code04-02.cpp
@@ -0,0 +1,43 @@
+#include "Common.h"
+
+typedef struct _Node
+{
+	string data;
+	struct _Node* link = NULL;
+} Node;
+
+int main()
+{
+	Node* head;
+
+	Node* node1 = new Node;
+	node1->data = "다현";
+	head = node1;
+
+	Node* node2 = new Node;
+	node2->data = "정연";
+	node1->link = node2;
+
+	Node* node3 = new Node;
+	node3->data = "쯔위";
+	node2->link = node3;
+
+	Node* node4 = new Node;
+	node4->data = "사나";
+	node3->link = node4;
+
+	Node* node5 = new Node;
+	node5->data = "지효";
+	node4->link = node5;
+
+	Node* current = head;
+	print(current->data);
+	while (current->link != NULL)
+	{
+		// 현재 데이터 이동
+		current = current->link;
+		print(current->data);
+	}
+
+	return 0;
+}
