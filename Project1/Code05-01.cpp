1569aebab598264f34117b6551aae07979df3860
wlgus123 <mail30019794@gmail.com>
2025-09-23 09:53:13 +0900

    원형 연결 리스트: 노드 삽입

--- Project1/Code05-01.cpp
+++ Project1/Code05-01.cpp
@@ -10,6 +10,47 @@ Array <Node*> memory;
 Node* head, * current, * pre;
 Array <string> dataArray = { "다현", "정연", "쯔위", "사나", "지효" };
 
+// 노드 삽입
+void insertNode(string findData, string insertData)
+{
+	Node* node;
+	Node* last;
+	if (head->data == findData)
+	{
+		node = new Node;
+		node->data = insertData;
+		node->link = head;
+		last = head;
+		while (last->link != head)
+			last = last->link;
+		last->link = node;
+		head = node;
+		memory.push_back(node);
+		return;
+	}
+	current = head;
+	while (current->link != head)
+	{
+		pre = current;
+		current = current->link;
+		if (current->data == findData)
+		{
+			node = new Node;
+			node->data = insertData;
+			node->link = current;
+			pre->link = node;
+			memory.push_back(node);
+			return;
+		}
+	}
+	node = new Node;
+	node->data = insertData;
+	current->link = node;
+	node->link = head;
+	memory.push_back(node);
+}
+
+// 노드 출력
 void printNodes(Node* start)
 {
 	if (start == NULL)
@@ -24,6 +65,7 @@ void printNodes(Node* start)
 	println("");
 }
 
+// 노드 할당 해제
 void freeMemory()
 {
 	for (int i = 0; i < len(memory); i++)
@@ -52,7 +94,16 @@ int main()
 		memory.push_back(node);
 	}
 
+	// 노드 삽입
+	insertNode("다현", "화사");	// 다현 앞에 화사 삽입
 	printNodes(head);
+
+	insertNode("사나", "솔라");	// 사나 앞에 솔라 삽입
+	printNodes(head);
+
+	insertNode("재남", "문별");	// 재남은 리스트에 없음. 따라서 맨 뒤에 삽입
+	printNodes(head);
+	
 	freeMemory();
 
 	return 0;
